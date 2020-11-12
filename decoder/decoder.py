5471259e07e47cd930bb2f3aeb0be48e93e8d3d8
Alan Mitchell <tabb99@gmail.com>
2020-11-12 10:06:29 -0900

    Improved identification of sensor type and fixed bug with flattening lists.

--- decoder/decoder.py
+++ decoder/decoder.py
@@ -58,9 +58,10 @@ def decode(
     # dispatch to the right decoding function based on characters in the device_id.
     # if device_id contains "lht65" anywhere in it, use the lht65 decoder
     # if device_id starts with "ers" or "elsys" or "elt", use the elsys decoder
-    if 'lht65' in device_id:
+    dev_id_lwr = device_id.lower()    # get variable for lower case device ID
+    if 'lht65' in dev_id_lwr:
         fields = decode_lht65.decode(payload)
-    elif device_id.startswith('elsys') or (device_id[:3] in ('ers', 'elt')):
+    elif dev_id_lwr.startswith('elsys') or (dev_id_lwr[:3] in ('ers', 'elt')):
         fields = decode_elsys.decode(payload)
     else:
         # no decoder for this payload
@@ -69,13 +70,34 @@ def decode(
     # some decoders will give a list of values back for one field.  If requested, convert 
     # these into multiple fields with an underscore index at end of field name.
     if flatten_value_lists:
+        flat_fields ={}
+        fields_to_delete = []
         for k, v in fields.items():
             if type(v) == list:
-                del fields[k]     # remove that item cuz will add individual elements
+                fields_to_delete.append(k)
                 for ix, val in enumerate(v):
-                    fields[f'{k}_{ix}'] = val
+                    flat_fields[f'{k}_{ix}'] = val
+        for k in fields_to_delete:
+            del fields[k]     # remove that item cuz will add individual elements
+        fields.update(flat_fields)
 
     # Add these fields to the results dictionary
     results['fields'] = fields
 
     return results
+
+def test():
+    from pprint import pprint
+    recs = [
+        {"app_id":"an-dragino","dev_id":"lht65-a84041000181c74e","hardware_serial":"A84041000181C74E","port":2,"counter":33041,"payload_raw":"yxH5YAKWAfl8f/8=","payload_fields":{"bat_v":2.833,"humidity":"66.2","temp_ext":"1.98","temp_int":"1.47"},"metadata":{"time":"2020-10-30T02:35:43.883078268Z","frequency":904.7,"modulation":"LORA","data_rate":"SF7BW125","coding_rate":"4/5","gateways":[{"gtw_id":"eui-a84041ffff1ee2b4","timestamp":3580480955,"time":"2020-10-30T02:35:43.828704Z","channel":4,"rssi":-90,"snr":8,"rf_chain":0}]},"downlink_url":"https://integrations.thethingsnetwork.org/ttn-us-west/api/v2/down/an-dragino/lora-signal?key=ttn-account-v2.vL5tZaRYIpvsQiQb8Iy48z6u0hqi3BIIaCS8yubFMgU"},
+        {"app_id":"sig-strength","dev_id":"elt-a81758fffe0523db","hardware_serial":"A81758FFFE0523DB","port":5,"counter":167,"payload_raw":"Bw4uDQE=","metadata":{"time":"2020-11-08T18:50:21.145287118Z","frequency":904.7,"modulation":"LORA","data_rate":"SF9BW125","coding_rate":"4/5","gateways":[{"gtw_id":"eui-58a0cbfffe8015fd","timestamp":1861174284,"time":"2020-11-08T18:50:21.086786985Z","channel":0,"rssi":-51,"snr":9.25,"rf_chain":0},{"gtw_id":"eui-a84041ffff1ee2b4","timestamp":471103348,"time":"2020-11-08T18:50:21.099979Z","channel":4,"rssi":-53,"snr":11,"rf_chain":0}]},"downlink_url":"https://integrations.thethingsnetwork.org/ttn-us-west/api/v2/down/sig-strength/lora-signal?key=ttn-account-v2.sgTyWsa1SlDoilFxQU6Q7TAWL80RhMYrM1dajv7Ej8k"},    
+        {"app_id":"sig-strength","dev_id":"elt-a81758fffe0523db","hardware_serial":"A81758FFFE0523DB","port":5,"counter":167,"payload_raw":"AQDiAikEACcFBgYDCAcNYhkAARn//w==","metadata":{"time":"2020-11-08T18:50:21.145287118Z","frequency":904.7,"modulation":"LORA","data_rate":"SF9BW125","coding_rate":"4/5","gateways":[{"gtw_id":"eui-58a0cbfffe8015fd","timestamp":1861174284,"time":"2020-11-08T18:50:21.086786985Z","channel":0,"rssi":-51,"snr":9.25,"rf_chain":0},{"gtw_id":"eui-a84041ffff1ee2b4","timestamp":471103348,"time":"2020-11-08T18:50:21.099979Z","channel":4,"rssi":-53,"snr":11,"rf_chain":0}]},"downlink_url":"https://integrations.thethingsnetwork.org/ttn-us-west/api/v2/down/sig-strength/lora-signal?key=ttn-account-v2.sgTyWsa1SlDoilFxQU6Q7TAWL80RhMYrM1dajv7Ej8k"},    
+    ]
+    for rec in recs:
+        pprint(decode(rec))
+
+    pprint(decode(recs[-1], flatten_value_lists=False))
+
+if __name__ == '__main__':
+    # To run this without import error, need to run "python -m decoder.decoder" from the top level directory.
+    test()
