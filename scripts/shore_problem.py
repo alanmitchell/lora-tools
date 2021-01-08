8e51a87e5f6ce55c5a2a7260f76eba989d6e54e0
Alan Mitchell <tabb99@gmail.com>
2021-01-08 07:23:53 -0900

    More shorepower script mods.

--- scripts/shore_problem.py
+++ scripts/shore_problem.py
@@ -12,19 +12,24 @@ def int16(ix: int) -> int:
     return (data[ix] << 8) | (data[ix + 1])
 
 bad_ct = 0
+fout = open('shore.tsv', 'w')
+fout.write('ts\tv1\tv2\n')
 for lin in open('shore.json'):
     rec = json.loads(lin.strip())
     data = base64.b64decode(rec['payload_raw'])
     v1 = int16(0) / 1000.
     v2 = int16(2) / 1000.
-    if rec['metadata']['time'] > '2021-01-05T00:00:00':
+    ts = rec['metadata']['time']
+    if ts > '2021-01-08T06:26:08':
         bad_data = False
-        if abs(v1) > 0.02 and abs(v1 - 6.18) > 0.05:
+        if abs(v2-0.579) > 0.02:
             bad_data = True
-        if abs(v2 - 11.92) > 0.1:
+        if abs(v1 - 11.88) > 0.1:
             bad_data = True
         if bad_data:
             print('***', end='')
             bad_ct += 1
-        print(f"{rec['metadata']['time']} V1: {v1:.3f}, V2:{v2:.3f}")
+        print(f"{ts} V1: {v1:.3f}, V2:{v2:.3f}")
+        fout.write(f'{ts}\t{v1}\t{v2}\n')
+fout.close()
 print(f'Number of Bad Readings: {bad_ct}')
