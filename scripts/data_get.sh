b354811291ff65e357966010f9dd43e7342bf230
Alan Mitchell <tabb99@gmail.com>
2021-01-11 19:43:30 -0900

    Updated data download script.

--- scripts/data_get.sh
+++ scripts/data_get.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-scp alan@akstats.com:/home/alan/an-api/lora-data/lora.txt lora.json
+scp alan@akstats.com:/home/alan/an-api/lora-data/lora.json lora.json
 grep A84041552182436A lora.json > lt22222.json
